65efba357dafa5144e7ddb5590a1866453ef30d6
Daniel Osokin <dosokin@1A5.42angouleme.fr>
2025-07-17 10:08:15 +0200

    readme update

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,9 +1,7 @@
 source .env
 
-export PATH="$PATH:/home/dosokin/.foundry/bin"
-
 curl --create-dirs -o ../lib/chainlink/interfaces/AggregatorV3Interface.sol https://raw.githubusercontent.com/smartcontractkit/chainlink-evm/refs/heads/develop/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol
 
-forge install OpenZeppelin/openzeppelin-contracts
+~/.foundry/bin/forge install OpenZeppelin/openzeppelin-contracts
 
-forge script --chain sepolia  Grillz.s.sol:GrillzScript --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY  --verify --broadcast
+~/.foundry/bin/forge script --chain sepolia  Grillz.s.sol:GrillzScript --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY # --verify --broadcast
